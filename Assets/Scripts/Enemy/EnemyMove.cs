bf91272670b720eb5063dd44d51db3ad888a2e58
HuKeqing <keqing.hu@icloud.com>
2020-04-12 14:41:18 +0800

    feat(Enemy): 增加了敌人的移动指令，当玩家在敌方单位的视野范围内时，敌人会朝着玩家走来

---
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyMove : MonoBehaviour
+    {
+        public Transform player;
+        public float moveSpeed;
+        public float viewRange;
+
+        private void Update()
+        {
+            var target = (player.position - transform.position).normalized;
+            if (!(Vector3.Dot(target, transform.forward) >= 0.866f)) return;
+            var ray = new Ray(transform.position, target);
+            if (!Physics.Raycast(ray, out var hitInfo, viewRange)) return;
+            if (hitInfo.collider.gameObject.layer != 10) return;
+            transform.LookAt(player);
+            transform.Translate(target * (moveSpeed * Time.deltaTime), Space.World);
+        }
+    }
+}
