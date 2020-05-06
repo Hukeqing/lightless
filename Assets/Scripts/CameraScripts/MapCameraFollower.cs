e54d8ed6799775610fbe17325f3d1f7fd2868a1c
HuKeqing <keqing.hu@icloud.com>
2020-05-06 16:45:06 +0800

    feat(MiniMap): 修改了小地图的渲染事件，现在的渲染效率更高了
    修改了房间门的位置大小，现在的房间门更加合理了

--- Assets/Scripts/CameraScripts/MapCameraFollower.cs
+++ Assets/Scripts/CameraScripts/MapCameraFollower.cs
@@ -9,30 +9,18 @@ namespace CameraScripts
         public float baseFlushTime;
 
         private Player.PlayerControl _pc;
-        private RenderTexture _lastTexture;
         private float _nextFlushTime;
 
         private void Start()
         {
             _pc = player.GetComponent<Player.PlayerControl>();
-            _lastTexture = new RenderTexture(GetComponent<Camera>().targetTexture.width,
-                GetComponent<Camera>().targetTexture.height, 0);
         }
 
         private void LateUpdate()
         {
+            if (!(Time.time > _nextFlushTime)) return;
             transform.position = player.position + new Vector3(0, 5, 0);
-        }
-
-        private void OnRenderImage(RenderTexture src, RenderTexture dest)
-        {
-            if (Time.time >= _nextFlushTime)
-            {
-                Graphics.Blit(src, _lastTexture);
-                _nextFlushTime = Time.time + baseFlushTime + 1 - _pc.HealthValue;
-            }
-
-            Graphics.Blit(_lastTexture, dest);
+            _nextFlushTime = Time.time + baseFlushTime + 1 - _pc.HealthValue;
         }
     }
 }
