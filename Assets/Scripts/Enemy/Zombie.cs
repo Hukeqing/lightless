a888e3fde5a4d574b18579a8e1a4fc10e7aa2ee3
Mauve <keqing.hu@icloud.com>
2020-06-09 10:02:28 +0800

    feat(Enemy): 增加了一个新的敌人
    修改了部分敌人的数据

---
+++ Assets/Scripts/Enemy/Zombie.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using Player;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class Zombie : Enemy
+    {
+        private PlayerControl _pc;
+        private Animator _zombieAnimator;
+
+        public int damage;
+        private static readonly int DieTrigger = Animator.StringToHash("Die");
+        private static readonly int DiePos = Animator.StringToHash("DiePos");
+
+        private void Start()
+        {
+            Init();
+            _pc = player.GetComponent<PlayerControl>();
+            _zombieAnimator = GetComponent<Animator>();
+            _zombieAnimator.SetBool(DiePos, Random.Range(0, 1.0f) < 0.5f);
+        }
+
+        protected override void Attack()
+        {
+            _pc.ApplyDamage(damage);
+        }
+
+        protected override void Die()
+        {
+            StartCoroutine(Dead());
+        }
+
+        private IEnumerator Dead()
+        {
+            _zombieAnimator.SetTrigger(DieTrigger);
+            yield return new WaitForSeconds(1.4f);
+            Destroy(gameObject);
+        }
+
+        public override void ApplyDamage(int getDamage)
+        {
+            if (curHealth <= 0) return;
+            curHealth -= getDamage;
+            if (curHealth > 0) return;
+            curHealth = 0;
+            Die();
+        }
+    }
+}
