efdb96b84347651e4c415a9538da0428a6d21457
Mauve <keqing.hu@icloud.com>
2020-06-13 20:02:41 +0800

    feat(Point): 增加了 PRD 算法，使得游戏更加公平

--- Assets/Scripts/Point/Point.cs
+++ Assets/Scripts/Point/Point.cs
@@ -12,16 +12,39 @@ namespace Point
 
         private GameObject _item;
 
+        /// <summary>
+        /// prd 算法，增加了游戏的平衡
+        /// </summary>
+        private static float _prdValue;
+
+        private static readonly float[] PrdUp =
+        {
+            0.000000000000000000000000000f, 0.003801658303553139101756466f, 0.014745844781072675877050816f,
+            0.032220914373087674975117359f, 0.055704042949781851858398652f, 0.084744091852316990275274806f,
+            0.118949192725403987583755553f, 0.157983098125747077557540462f, 0.201547413607754017070679639f,
+            0.249306998440163189714677100f, 0.302103025348741965169160432f, 0.360397850933168697104686803f,
+            0.422649730810374235490851220f, 0.481125478337229174401911323f, 0.571428571428571428571428572f,
+            0.666666666666666666666666667f, 0.750000000000000000000000000f, 0.823529411764705882352941177f,
+            0.888888888888888888888888889f, 0.947368421052631578947368421f, 1.000000000000000000000000000f
+        };
+
+        private static int RandomPos(float value)
+        {
+            return Mathf.RoundToInt(value * 20);
+        }
+
         private void Start()
         {
             var tmp = Random.Range(0.0f, 1.0f);
-            if (tmp < itemValue)
+            if (tmp < _prdValue)
             {
                 InsItem();
+                _prdValue = PrdUp[RandomPos(itemValue)];
             }
             else
             {
                 InsEnemy();
+                _prdValue += PrdUp[RandomPos(itemValue)];
             }
         }
 
